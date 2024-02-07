fedea5d5ae2ea658238f83f80f5d8e16d66e62bd
oliver <lifefossil@outlook.com>
2024-02-07 14:47:39 +0800

    add pyinstaler

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import argparse
 from src.core.params import pyproject_toml_params
 parser = argparse.ArgumentParser(description=pyproject_toml_params.description)
+parser.add_argument("--version", "-v", action="version", help="版本信息", version=pyproject_toml_params.version)
 
+args = parser.parse_args()
 
+print(args)
 def start():
     """
     项目开始入口
